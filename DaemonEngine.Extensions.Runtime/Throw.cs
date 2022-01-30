5bca503a8bf4b0b094fadf17a589294aacedb6e3
Victor Krogh Hansen <kingodk@outlook.dk>
2022-01-30 10:26:40 +0100

    Added Throw class

---
+++ DaemonEngine.Extensions.Runtime/Throw.cs
@@ -0,0 +1,12 @@
+﻿namespace DaemonEngine.Extensions.Runtime;
+
+public static class Throw
+{
+    public static void IfNull(object? obj, string parameterName)
+    {
+        if (obj == null)
+        {
+            throw new ArgumentNullException(nameof(obj));
+        }
+    }
+}
