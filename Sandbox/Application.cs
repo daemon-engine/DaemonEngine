8ac83c9b6409b596fa7bc218f66813e9f4106a04
Victor Krogh Hansen <kingodk@outlook.dk>
2022-01-30 13:07:29 +0100

    Add new flat color shader

--- Sandbox/Application.cs
+++ Sandbox/Application.cs
@@ -21,25 +21,26 @@ public class Application : ApplicationBase
     {
         Logger.Information("OnStart");
 
-        float[] vertices = new float[4 * 3] {
-             0.5f,  0.5f, 0.0f,  // top right
-             0.5f, -0.5f, 0.0f,  // bottom right
-            -0.5f, -0.5f, 0.0f,  // bottom left
-            -0.5f,  0.5f, 0.0f   // top left 
+        float[] vertices = new float[4 * (3 + 3)] {
+             0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 0.0f,  // top right
+             0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f,  // bottom right
+            -0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f,  // bottom left
+            -0.5f,  0.5f, 0.0f, 1.0f, 1.0f, 1.0f   // top left 
         };
         uint[] indices = new uint[2 * 3] {  // note that we start from 0!
             0, 1, 3,   // first triangle
             1, 2, 3    // second triangle
         };
 
-        var shader = GraphicsFactory.CreateShader("Assets/Shaders/Basic.shader");
+        var shader = GraphicsFactory.CreateShader("Assets/Shaders/FlatColor.shader");
 
         var layout = new BufferLayout(new List<BufferElement>
         {
-            new BufferElement("POSITION", ShaderDataType.Float3)
+            new BufferElement("POSITION", ShaderDataType.Float3),
+            new BufferElement("COLOR", ShaderDataType.Float3)
         });
         _pipeline = GraphicsFactory.CreatePipeline(shader, layout);
-        _vertexBuffer = GraphicsFactory.CreateVertexBuffer(12 * sizeof(float), vertices);
+        _vertexBuffer = GraphicsFactory.CreateVertexBuffer(24 * sizeof(float), vertices);
         _ibo = GraphicsFactory.CreateIndexBuffer(6, indices);
     }
 
---
+++ Sandbox/Assets/Shaders/FlatColor.shader
@@ -0,0 +1,23 @@
+﻿#type vertex
+#version 330 core
+layout(location = 0) in vec3 POSITION;
+layout(location = 1) in vec3 COLOR;
+
+out vec3 _Color;
+
+void main()
+{
+    gl_Position = vec4(POSITION, 1.0);
+    _Color = COLOR;
+}
+
+#type fragment
+#version 330 core
+out vec4 FragColor;
+
+in vec3 _Color;
+
+void main()
+{
+    FragColor = vec4(_Color, 1.0f);
+}
