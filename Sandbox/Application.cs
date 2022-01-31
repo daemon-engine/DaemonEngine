137247ce8e38839d0dc4f9717dda4092e7bd3a59
Victor Krogh Hansen <kingodk@outlook.dk>
2022-01-31 19:15:27 +0100

    Added Plane generation method

--- Sandbox/Application.cs
+++ Sandbox/Application.cs
@@ -32,17 +32,6 @@ public class Application : ApplicationBase
         var cubeVertices = Builder.GenerateCubeVertices();
         var cubeIndices = Builder.GenerateCubeIndices();
 
-        //float[] vertices = new float[4 * (3 + 2)] {
-        //     0.5f,  0.5f, 0.0f, 1.0f, 1.0f,  // top right
-        //     0.5f, -0.5f, 0.0f, 1.0f, 0.0f,  // bottom right
-        //    -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,  // bottom left
-        //    -0.5f,  0.5f, 0.0f, 0.0f, 1.0f   // top left 
-        //};
-        //uint[] indices = new uint[2 * 3] {  // note that we start from 0!
-        //    0, 1, 3,   // first triangle
-        //    1, 2, 3    // second triangle
-        //};
-
         _shader = GraphicsFactory.CreateShader("Assets/Shaders/CoordinateSystem.shader");
 
         _texture1 = GraphicsFactory.CreateTexture("Assets/Textures/container.jpg");
--- Sandbox/Builder.cs
+++ Sandbox/Builder.cs
@@ -2,38 +2,61 @@
 
 public static class Builder
 {
-	public static uint[] GenerateCubeIndices()
+    public static uint[] GeneratePlaneIndices()
     {
-		uint[] indices = new uint[12 * 3]
-		{
-			// Back
-			0, 1, 2,
-			2, 3, 0,
-
-			// Front
-			4, 5, 6,
-			6, 7, 4,
-
-			// Right
-			8, 9, 10,
-			10, 11, 8,
-
-			// Left
-			12, 13, 14,
-			14, 15, 12,
-
-			// Top
-			16, 17, 18,
-			18, 19, 16,
-
-			// Bottom
-			20, 21, 22,
-			22, 23, 20,
+		uint[] indices = new uint[2 * 3] {
+			0, 1, 3,
+			1, 2, 3
 		};
+
 		return indices;
     }
 
-	public static float[] GenerateCubeVertices()
+    public static float[] GeneratePlaneVertices()
+    {
+        float[] vertices = new float[4 * (3 + 2)] 
+		{
+             0.5f,  0.5f, 0.0f, 1.0f, 1.0f,
+			 0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+			-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+			-0.5f,  0.5f, 0.0f, 0.0f, 1.0f
+		};
+
+        return vertices;
+    }
+
+    public static uint[] GenerateCubeIndices()
+    {
+        uint[] indices = new uint[12 * 3]
+        {
+			// Back
+			0, 1, 2,
+            2, 3, 0,
+
+			// Front
+			4, 5, 6,
+            6, 7, 4,
+
+			// Right
+			8, 9, 10,
+            10, 11, 8,
+
+			// Left
+			12, 13, 14,
+            14, 15, 12,
+
+			// Top
+			16, 17, 18,
+            18, 19, 16,
+
+			// Bottom
+			20, 21, 22,
+            22, 23, 20,
+        };
+        return indices;
+    }
+
+    public static float[] GenerateCubeVertices()
     {
         float[] vertices = new float[24 * (3 + 2)]
         {
@@ -73,6 +96,6 @@ public static class Builder
 			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f, //22
 			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f, //23
         };
-		return vertices;
+        return vertices;
     }
 }
