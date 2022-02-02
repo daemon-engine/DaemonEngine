9bc01b95838e565b771eb604e95cf8ae96272240
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-02 18:46:52 +0100

    Removed out-commented code

--- Sandbox/Program.cs
+++ Sandbox/Program.cs
@@ -1,17 +1,9 @@
 ﻿using DaemonEngine.DependencyInjection;
 using DaemonEngine.Graphics.DependencyInjection;
 using Sandbox;
-using Sandbox.Layers;
 
 var builder = new DaemonEngineContainerBuilder();
 builder.RegisterApplication<Application>()
     .RegisterGraphicsModule(DaemonEngine.Graphics.Renderer.RendererApi.OpenGL)
     .RegisterLogging()
     .BuildAndRun();
-
-/*
-builder.RegisterApplication<Application>()
-    .RegisterGraphicsModule(RendererApi)
-    .RegisterLogging()
-    .BuildAndRun();
- */
