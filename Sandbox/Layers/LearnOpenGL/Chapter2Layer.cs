2b4906395f9d504389360ede9c005003df2cf263
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-07 19:57:45 +0100

    Added shader for multiple light casters

--- Sandbox/Layers/LearnOpenGL/Chapter2Layer.cs
+++ Sandbox/Layers/LearnOpenGL/Chapter2Layer.cs
@@ -28,8 +28,6 @@ internal class Chapter2Layer : LayerBase
 
     FPSCamera _camera;
 
-    private Vector3 _pointLightPosition = Vector3.Zero;
-
     private readonly IApplication _application;
     private readonly ICursor _cursor;
 
@@ -45,6 +43,13 @@ internal class Chapter2Layer : LayerBase
         new Vector3( 1.5f,  2.0f, -2.5f),
         new Vector3( 1.5f,  0.2f, -1.5f),
         new Vector3(-1.3f,  1.0f, -1.5f)
+    };
+
+    Vector3[] _pointLightPositions = {
+        new Vector3( 0.7f,  0.2f,  2.0f),
+        new Vector3( 2.3f, -3.3f, -4.0f),
+        new Vector3(-4.0f,  2.0f, -12.0f),
+        new Vector3( 0.0f,  0.0f, -3.0f)
     };
 
     public Vector3 CopperAmbient = new(0.19125f, 0.0735f, 0.0225f);
@@ -68,7 +73,7 @@ internal class Chapter2Layer : LayerBase
         _container = GraphicsFactory.CreateTexture("Assets/Textures/container2.png");
         _containerSpecular = GraphicsFactory.CreateTexture("Assets/Textures/container2_specular.png");
 
-        _lightingShader = GraphicsFactory.CreateShader("Assets/Shaders/LearnOpenGL/Chapter2/LightCasters_SpotLight.shader");
+        _lightingShader = GraphicsFactory.CreateShader("Assets/Shaders/LearnOpenGL/Chapter2/MultipleLightCasters.shader");
         _lightObjectShader = GraphicsFactory.CreateShader("Assets/Shaders/LearnOpenGL/Chapter2/1.LightCube.shader");
 
         var layout = new BufferLayout(new List<BufferElement>
@@ -114,50 +119,77 @@ internal class Chapter2Layer : LayerBase
         _lightingShader.SetMat4("_View", _camera.ViewMatrix);
         _lightingShader.SetMat4("_Projection", _camera.ProjectionMatrix);
         _lightingShader.SetFloat3("_ViewPos", _camera.Position.X, _camera.Position.Y, _camera.Position.Z);
+        _lightingShader.SetFloat("_Material.shininess", 32.0f);
 
+        _lightingShader.SetFloat3("_DirLight.direction", -0.2f, -1.0f, -0.3f);
+        _lightingShader.SetFloat3("_DirLight.ambient", 0.05f, 0.05f, 0.05f);
+        _lightingShader.SetFloat3("_DirLight.diffuse", 0.4f, 0.4f, 0.4f);
+        _lightingShader.SetFloat3("_DirLight.specular", 0.5f, 0.5f, 0.5f);
+
+        for (int i = 0; i < _pointLightPositions.Length; i++)
+        {
+            SetPointLight(i);
+        }
+
+        // spotLight
         _lightingShader.SetFloat3("_SpotLight.position", _camera.Position.X, _camera.Position.Y, _camera.Position.Z);
         _lightingShader.SetFloat3("_SpotLight.direction", _camera.Front.X, _camera.Front.Y, _camera.Front.Z);
-        _lightingShader.SetFloat3("_SpotLight.ambient", 0.1f, 0.1f, 0.1f);
-        _lightingShader.SetFloat3("_SpotLight.diffuse", 0.8f, 0.8f, 0.8f);
+        _lightingShader.SetFloat3("_SpotLight.ambient", 0.0f, 0.0f, 0.0f);
+        _lightingShader.SetFloat3("_SpotLight.diffuse", 1.0f, 1.0f, 1.0f);
         _lightingShader.SetFloat3("_SpotLight.specular", 1.0f, 1.0f, 1.0f);
-        _lightingShader.SetFloat("_SpotLight.cutOff", (float)Math.Cos(12.5f * 0.01745329251f));
-        _lightingShader.SetFloat("_SpotLight.outerCutOff", (float)Math.Cos(17.5f * 0.01745329251f));
-
         _lightingShader.SetFloat("_SpotLight.constant", 1.0f);
         _lightingShader.SetFloat("_SpotLight.linear", 0.09f);
         _lightingShader.SetFloat("_SpotLight.quadratic", 0.032f);
+        _lightingShader.SetFloat("_SpotLight.cutOff", (float)Math.Cos(12.5f * 0.01745329251f));
+        _lightingShader.SetFloat("_SpotLight.outerCutOff", (float)Math.Cos(15.0f * 0.01745329251f));
 
-        _lightingShader.SetFloat("_Material.shininess", 32.0f);
+        var model = Matrix4x4.Identity;
 
         _container.Bind();
         _containerSpecular.Bind(1);
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < _cubePositions.Length; i++)
         {
             float angle = 20.0f * i;
 
-            Matrix4x4 model = Matrix4x4.Identity
-                * Matrix4x4.CreateTranslation(_cubePositions[i])
+            model = Matrix4x4.Identity
                 * Matrix4x4.CreateRotationX(angle * 0.01745329251f)
                 * Matrix4x4.CreateRotationY(angle * 0.01745329251f)
-                * Matrix4x4.CreateRotationZ(angle * 0.01745329251f);
+                * Matrix4x4.CreateRotationZ(angle * 0.01745329251f)
+                * Matrix4x4.CreateTranslation(_cubePositions[i]);
 
-            _lightingShader.Bind();
             _lightingShader.SetMat4("_Model", model);
-
             Renderer.RenderGeometry(_lightingPipeline, _vertexBuffer, _indexBuffer);
         }
 
-        var lightObjectModel = Matrix4x4.Identity
-            * Matrix4x4.CreateScale(0.2f)
-            * Matrix4x4.CreateTranslation(_pointLightPosition);
-
+        // Point light cubes
         _lightObjectShader.Bind();
-        _lightObjectShader.SetMat4("_Model", lightObjectModel);
+        _lightObjectShader.SetMat4("_Model", Matrix4x4.Identity);
         _lightObjectShader.SetMat4("_View", _camera.ViewMatrix);
         _lightObjectShader.SetMat4("_Projection", _camera.ProjectionMatrix);
 
-        Renderer.RenderGeometry(_lightObjectPipeline, _vertexBuffer, _indexBuffer);
+        for (int i = 0; i < _pointLightPositions.Length; i++)
+        {
+            model = Matrix4x4.Identity
+                * Matrix4x4.CreateScale(0.2f)
+                * Matrix4x4.CreateTranslation(_pointLightPositions[i]);
+
+            _lightObjectShader.SetMat4("_Model", model);
+
+            Renderer.RenderGeometry(_lightObjectPipeline, _vertexBuffer, _indexBuffer);
+        }
+    }
+
+    private void SetPointLight(int index)
+    {
+        _lightingShader.Bind();
+        _lightingShader.SetFloat3($"_PointLights[{index}].position", _pointLightPositions[index].X, _pointLightPositions[index].Y, _pointLightPositions[index].Z);
+        _lightingShader.SetFloat3($"_PointLights[{index}].ambient", 0.05f, 0.05f, 0.05f);
+        _lightingShader.SetFloat3($"_PointLights[{index}].diffuse", 0.8f, 0.8f, 0.8f);
+        _lightingShader.SetFloat3($"_PointLights[{index}].specular", 1.0f, 1.0f, 1.0f);
+        _lightingShader.SetFloat($"_PointLights[{index}].constant", 1.0f);
+        _lightingShader.SetFloat($"_PointLights[{index}].linear", 0.09f);
+        _lightingShader.SetFloat($"_PointLights[{index}].quadratic", 0.032f);
     }
 
     public override void OnGUI()
@@ -165,13 +197,11 @@ internal class Chapter2Layer : LayerBase
         var io = ImGuiNET.ImGui.GetIO();
 
         ImGuiNET.ImGui.Begin("Performace");
+
         ImGuiNET.ImGui.Text($"State: {(s_MovedDisabled ? "Playing" : "Paused")}");
         ImGuiNET.ImGui.Text($"Delta Time: {(io.DeltaTime * 1000.0f):f4}ms/frame");
         ImGuiNET.ImGui.Text($"FPS: {1.0f / io.DeltaTime:f1}");
 
-        ImGuiNET.ImGui.Spacing();
-
-        ImGuiNET.ImGui.SliderFloat3("Light", ref _pointLightPosition, -10.0f, 10.0f);
         ImGuiNET.ImGui.End();
 
         ImGuiNET.ImGui.ShowDemoWindow();
