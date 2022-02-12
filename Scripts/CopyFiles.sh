c88f0e6c107cf7cdc80dec6fe1148e5eb07fabb3
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-12 16:17:51 +0100

    Generated script

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
+# cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
+cp -fRu ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/*
