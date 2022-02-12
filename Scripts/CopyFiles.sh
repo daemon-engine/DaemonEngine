895b05f60f769430041fc06e55cb79cd50403dea
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-12 16:20:07 +0100

    Full paths

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
-cp -fRu ../Sandbox/Assets ../bin/Release/net6.0/Assets
+cp -fRu /workspace/gine_feature_jenkins_integration/Sandbox/Assets /workspace/gine_feature_jenkins_integration/Sandbox/bin/Release/net6.0/Assets
