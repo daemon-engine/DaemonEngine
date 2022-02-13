4799c89009f168a4d5092dbb8bc1acfc0ea9b251
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 12:07:19 +0100

    Small changes

--- Scripts/ZipOutput.sh
+++ Scripts/ZipOutput.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-zip -r DaemonEngine.zip Sandbox/Assets Sandbox/bin/Release/net6.0-windows/win10-x64
+zip -r DaemonEngine.zip Sandbox/Assets Sandbox/bin/Release/net6.0-windows/win10-x64/**
 
