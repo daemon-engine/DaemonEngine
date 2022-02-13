44afad4928065a2cca6d213c6956a1a8879ff83f
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 11:32:40 +0100

    Fixed output path

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
  
 # Copy Assets to Output
-cp -f -R -u -a Sandbox/Assets Sandbox/bin/Release/net6.0/Assets
+cp -f -R -u -a Sandbox/Assets Sandbox/bin/Release/net6.0-windows/Assets
 
 # Copy glfw3.dll file to Output
-mkdir Sandbox/bin/Release/net6.0/Binary
-cp -f -u -a DaemonEngine.Windows.GraphicsLibraryFramework.DllImport/Binary/glfw3.dll Sandbox/bin/Release/net6.0/Binary/glfw3.dll
+mkdir Sandbox/bin/Release/net6.0-windows/Binary
+cp -f -u -a DaemonEngine.Windows.GraphicsLibraryFramework.DllImport/Binary/glfw3.dll Sandbox/bin/Release/net6.0-windows/Binary/glfw3.dll
 
