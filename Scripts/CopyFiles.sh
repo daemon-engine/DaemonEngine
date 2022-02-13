b7f42a6fa8a194fdfb9ff69afb6be52d3e94a8f5
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 11:10:47 +0100

    test

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
-cp -f -R -u -a ../Sandbox/Assets ../Sandbox/bin/Release/net6.0/Assets
+cp -f -R -u -a Sandbox/Assets Sandbox/bin/Release/net6.0/Assets
+# cp -f -R -u -a ../Sandbox/Assets ../Sandbox/bin/Release/net6.0/Assets
