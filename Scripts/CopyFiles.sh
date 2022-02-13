07342ceddfc65a43e2792e34c6f3ac42c92e83db
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 11:16:23 +0100

    Added copy of glfw3.dll file to output

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-
-# cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
+ 
+# Copy Assets to Output
 cp -f -R -u -a Sandbox/Assets Sandbox/bin/Release/net6.0/Assets
-# cp -f -R -u -a ../Sandbox/Assets ../Sandbox/bin/Release/net6.0/Assets
+
+# Copy glfw3.dll file to Output
+cp -f -u -a DaemonEngine.Windows.GraphicsLibraryFramework.DllImport/Binary/glfw3.dll Sandbox/bin/Release/net6.0/Binary/glfw3.dll
+
