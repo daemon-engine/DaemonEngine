043a350dfe52bb59623651247f9a9fa7a2f30a58
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 12:14:52 +0100

    Small changes

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -4,6 +4,6 @@
 cp -f -R -u -a Sandbox/Assets Output/Assets
 
 # Copy glfw3.dll file to Output
-mkdir Sandbox/bin/Release/net6.0-windows/win10-x64/Binary
+mkdir Output/Binary
 cp -f -u -a DaemonEngine.Windows.GraphicsLibraryFramework.DllImport/Binary/glfw3.dll Output/Binary/glfw3.dll
 
