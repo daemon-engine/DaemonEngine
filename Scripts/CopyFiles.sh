d38c6060a9bc7ea89546d187da2cf7ef41cb6889
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-13 11:09:39 +0100

    Small changes

--- Scripts/CopyFiles.sh
+++ Scripts/CopyFiles.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # cp -F -U -R ../Sandbox/Assets/* ../bin/Release/net6.0/Assets/* 
-cp -f -R -u -a /workspace/gine_feature_jenkins_integration/Sandbox/Assets /workspace/gine_feature_jenkins_integration/Sandbox/bin/Release/net6.0/Assets
+cp -f -R -u -a ../Sandbox/Assets ../Sandbox/bin/Release/net6.0/Assets
