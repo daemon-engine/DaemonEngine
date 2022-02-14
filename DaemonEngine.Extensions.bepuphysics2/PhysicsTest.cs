72cf7b4b88e8295b0ac0fe1871dd6d0bad2cfc85
Victor Krogh Hansen <kingodk@outlook.dk>
2022-02-14 12:12:18 +0100

    Trying to get bepuphysics2 to work...

---
+++ DaemonEngine.Extensions.bepuphysics2/PhysicsTest.cs
@@ -0,0 +1,145 @@
+﻿using BepuUtilities;
+using BepuUtilities.Memory;
+using BepuPhysics;
+using BepuPhysics.Collidables;
+using BepuPhysics.CollisionDetection;
+using BepuPhysics.Constraints;
+using System;
+using System.Runtime.CompilerServices;
+using System.Numerics;
+namespace DaemonEngine.Extensions.bepuphysics2;
+
+internal struct TestPoseIntegratorCallbacks : IPoseIntegratorCallbacks
+{
+    private readonly float _linearDamping;
+    private readonly float _angularDamping;
+
+    public TestPoseIntegratorCallbacks(Vector3 gravity, float linearDamping = 0.03f, float angularDamping = 0.03f)
+    {
+        Gravity = gravity;
+        _linearDamping = linearDamping;
+        _angularDamping = angularDamping;
+    }
+
+    public Vector3 Gravity { get; set; }
+    public AngularIntegrationMode AngularIntegrationMode => AngularIntegrationMode.Nonconserving;
+
+    public void Initialize(Simulation simulation)
+    {
+    }
+
+    public void PrepareForIntegration(float dt)
+    {
+    }
+
+    public void IntegrateVelocity(int bodyIndex, in RigidPose pose, in BodyInertia localInertia, int workerIndex, ref BodyVelocity velocity)
+    {
+        velocity.Linear = (velocity.Linear + new Vector3(0.0f, -9.81f, 0.0f));
+    }
+}
+
+internal struct TestNarrowPhaseCallbacks : INarrowPhaseCallbacks
+{
+    public TestNarrowPhaseCallbacks()
+    {
+        ContactSpringiness = new SpringSettings(30.0f, 1.0f);
+        MaximumRecoveryVelocity = 2.0f;
+        FrictionCoefficient = 1.0f;
+    }
+
+    public SpringSettings ContactSpringiness { get; set; }
+    public float MaximumRecoveryVelocity { get; set; }
+    public float FrictionCoefficient { get; set; }
+
+    public void Initialize(Simulation simulation)
+    {
+        //Use a default if the springiness value wasn't initialized... at least until struct field initializers are supported outside of previews.
+        if (ContactSpringiness.AngularFrequency == 0 && ContactSpringiness.TwiceDampingRatio == 0)
+        {
+            ContactSpringiness = new(30, 1);
+            MaximumRecoveryVelocity = 2f;
+            FrictionCoefficient = 1f;
+        }
+    }
+
+    public bool AllowContactGeneration(int workerIndex, CollidableReference a, CollidableReference b)
+    {
+        return a.Mobility == CollidableMobility.Dynamic || b.Mobility == CollidableMobility.Dynamic;
+    }
+
+    public bool AllowContactGeneration(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB)
+    {
+        return true;
+    }
+
+    public bool ConfigureContactManifold<TManifold>(int workerIndex, CollidablePair pair, ref TManifold manifold, out PairMaterialProperties pairMaterial) where TManifold : struct, IContactManifold<TManifold>
+    {
+        pairMaterial.FrictionCoefficient = FrictionCoefficient;
+        pairMaterial.MaximumRecoveryVelocity = MaximumRecoveryVelocity;
+        pairMaterial.SpringSettings = ContactSpringiness;
+        return true;
+    }
+
+    public bool ConfigureContactManifold(int workerIndex, CollidablePair pair, int childIndexA, int childIndexB, ref ConvexContactManifold manifold)
+    {
+        return true;
+    }
+
+    public void Dispose()
+    {
+    }
+}
+
+internal class DefaultTimestepper : ITimestepper
+{
+    /// <summary>
+    /// Fires after the sleeper completes and before bodies are integrated.
+    /// </summary>
+    public event TimestepperStageHandler Slept;
+    /// <summary>
+    /// Fires after bodies have their bounding boxes updated for the frame's predicted motion and before collision detection.
+    /// </summary>
+    public event TimestepperStageHandler BeforeCollisionDetection;
+    /// <summary>
+    /// Fires after all collisions have been identified, but before the substep loop begins.
+    /// </summary>
+    public event TimestepperStageHandler CollisionsDetected;
+    /// <summary>
+    /// Fires after the solver executes and before the final integration step.
+    /// </summary>
+    public event TimestepperStageHandler ConstraintsSolved;
+
+    public void Timestep(Simulation simulation, float dt, IThreadDispatcher threadDispatcher = null)
+    {
+        simulation.Sleep(threadDispatcher);
+        Slept?.Invoke(dt, threadDispatcher);
+
+        simulation.PredictBoundingBoxes(dt, threadDispatcher);
+        BeforeCollisionDetection?.Invoke(dt, threadDispatcher);
+
+        simulation.CollisionDetection(dt, threadDispatcher);
+        CollisionsDetected?.Invoke(dt, threadDispatcher);
+
+        simulation.Solve(dt, threadDispatcher);
+        ConstraintsSolved?.Invoke(dt, threadDispatcher);
+
+        simulation.IncrementallyOptimizeDataStructures(threadDispatcher);
+    }
+}
+
+public class PhysicsTest
+{
+    public PhysicsTest()
+    {
+        var bufferPool = new BufferPool();
+        Simulation = Simulation.Create(bufferPool, new TestNarrowPhaseCallbacks(), new TestPoseIntegratorCallbacks(), new DefaultTimestepper());
+    }
+
+    public Simulation Simulation { get; }
+
+    public void Update()
+    {
+        const float timeToSimulate = 1 / 60f;
+        Simulation.Timestep(timeToSimulate);
+    }
+}
---
+++ Sandbox/Layers/PhysicsTestLayer.cs
@@ -0,0 +1,85 @@
+﻿using BepuPhysics;
+using BepuPhysics.Collidables;
+using DaemonEngine.Core.Layer;
+using DaemonEngine.Extensions.bepuphysics2;
+using DaemonEngine.Graphics.Factories;
+using DaemonEngine.Graphics.Renderer;
+using DaemonEngine.Mathematics;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Sandbox.Layers;
+
+internal class PhysicsTestLayer : LayerBase
+{
+    private IShader _shader;
+
+    private Model _plane;
+    private Model _cube;
+    private BodyDescription _cubeBody;
+
+    private Vector3 _lightDirection = new(-0.2f, -1.0f, -0.3f);
+    private FPSCamera _camera;
+
+    public PhysicsTestLayer(string name, IServiceProvider serviceProvider) 
+        : base(name, serviceProvider)
+    {
+    }
+
+    protected PhysicsTest Physics { get; set; }
+
+    public override void OnStart()
+    {
+        _camera = new FPSCamera(60.0f, Window.AspectRatio);
+
+        Physics = new PhysicsTest();
+
+        var planeShape = Physics.Simulation.Shapes.Add(new Box(10.0f, 1.0f, 10.0f));
+        Physics.Simulation.Statics.Add(new StaticDescription(new System.Numerics.Vector3(0.0f, 0.0f, 0.0f), planeShape, 0.01f));
+
+        var cubeShape = new Box(1.0f, 1.0f, 1.0f);
+        cubeShape.ComputeInertia(1.0f, out BodyInertia bodyInertia);
+        var collidableDescription = new CollidableDescription(Physics.Simulation.Shapes.Add(cubeShape), 0.01f);
+        var bodyActivityDescription = new BodyActivityDescription(1.0f);
+        _cubeBody = BodyDescription.CreateDynamic(new Vector3(0.0f, 5.0f, 0.0f), bodyInertia, collidableDescription, bodyActivityDescription);
+
+        var meshFactory = ServiceProvider.GetRequiredService<IMeshFactory>();
+        _shader = GraphicsFactory.CreateShader("Assets/Shaders/LearnOpenGL/Chapter3/LitBasic.shader");
+        _plane = new Model(meshFactory, _shader, "Assets/Models/Plane/plane.obj");
+        _cube = new Model(meshFactory, _shader, "Assets/Models/cube.obj");
+    }
+
+    public override void OnShutdown()
+    {
+    }
+
+    public override void OnUpdate(float deltaTime)
+    {
+        _camera.Update(deltaTime);
+
+        Physics.Update();
+
+        Renderer.Clear(ClearMask.ColorBufferBit | ClearMask.DepthBufferBit);
+        Renderer.ClearColor(0.3f, 0.5f, 0.8f, 1.0f);
+
+        _shader.Bind();
+        _shader.SetMat4("_View", _camera.ViewMatrix);
+        _shader.SetMat4("_Projection", _camera.ProjectionMatrix);
+        _shader.SetFloat3("_ViewPos", _camera.Position);
+
+        _shader.SetFloat3("_DirectionalLight.direction", _lightDirection);
+        _shader.SetFloat3("_DirectionalLight.ambient", 0.05f, 0.05f, 0.05f);
+        _shader.SetFloat3("_DirectionalLight.diffuse", 0.4f, 0.4f, 0.4f);
+        _shader.SetFloat3("_DirectionalLight.specular", 1.0f, 1.0f, 1.0f);
+
+        _shader.SetFloat3("_Material.ambient", new Vector3(0.24725f, 0.1995f, 0.0745f));
+        _shader.SetFloat3("_Material.diffuse", new Vector3(0.75164f, 0.60648f, 0.22648f));
+        _shader.SetFloat3("_Material.specular", new Vector3(0.628281f, 0.555802f, 0.366065f));
+        _shader.SetFloat("_Material.shininess", 0.4f * 128.0f);
+
+        _shader.SetMat4("_Model", Matrix4.Identity);
+        Renderer.RenderMesh(_plane.Mesh);
+
+        _shader.SetMat4("_Model", Matrix4.Identity * Matrix4.Translate(_cubeBody.Pose.Position));
+        Renderer.RenderMesh(_cube.Mesh);
+    }
+}
